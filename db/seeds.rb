f8dab9ebec3ddd81989276a0661ab75ce57f980c
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-27 22:28:50 -0500

    commit tree 92b8c6cbf73c360422b925034067deeb1f8d6cac

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Recipe.create(:name => "waldorf salad", :ingredients => "apples, cabbage, oil, vinegar", :cook_time => "0")
+Recipe.create(:name => "Bomb.com Mac and Cheese", :ingredients => "milk, butter, cheese, elbow pasta", :cook_time => "20 minutes")
+Recipe.create(:name => "Enchiladas con Salsa Verde", :ingredients => "Tortillas, Queso Blanco, Tomatillos, Onion, Garlic, Black beans, Cilantro", :cook_time => "20 minutes")
